2e9753097efb50b341a33e18a698de6835611f4b
yutaro_nosaka <>
2024-05-24 00:42:20 +0900

    [add] sort問題

---
+++ LeetCode/src/main/kotlin/sorting/SortTheStudentsByTheirKthScore.kt
@@ -0,0 +1,16 @@
+package sorting
+
+class SortTheStudentsByTheirKthScore {
+    class Solution {
+        /*
+        Input: score = [[10,6,9,1],[7,5,11,2],[4,8,3,15]], k = 2
+        Output: [[7,5,11,2],[10,6,9,1],[4,8,3,15]]
+         */
+        fun sortTheStudents(score: Array<IntArray>, k: Int): Array<IntArray> {
+            val length = score.size
+            val pivotIndex = (length - 1) / 2
+            val pivot = score[pivotIndex][k]
+            for (i in )
+        }
+    }
+}
